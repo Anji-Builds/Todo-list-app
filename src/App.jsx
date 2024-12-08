7f4848727794c5f07975b4f2f43dfddec4dd42d9
Anji-Builds <anjalibiju0504@gmail.com>
2024-12-08 22:37:48 +0100

    storing the list of items into local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from "./components/Header"
 import Form from './components/Form';
 import Todoslist from './components/Todoslist';
@@ -6,11 +6,13 @@ import './App.css';
 import { use } from 'react';
 
 const App = () => {
-
+  const initialState = JSON.parse(localStorage.getItem("todos")) || []
   const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
 
+  useEffect(() =>{localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
   return (
     <div className="container">
       <div className="app-wrapper">
