ff6de8b0aa81164f326b4b25d3618f3720a05ed6
Anji-Builds <anjalibiju0504@gmail.com>
2024-12-08 22:22:31 +0100

    added edit, complete and delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import Header from "./components/Header"
 import Form from './components/Form';
 import Todoslist from './components/Todoslist';
 import './App.css';
+import { use } from 'react';
 
 const App = () => {
 
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+
   return (
     <div className="container">
       <div className="app-wrapper">
@@ -20,10 +23,16 @@ const App = () => {
             setInput = {setInput}
             todos = {todos}
             setTodos = {setTodos}
+            editTodo = {editTodo}
+            setEditTodo = {setEditTodo}
           />
         </div> 
         <div>
-          <Todoslist todos={todos} setTodos={setTodos} />
+          <Todoslist 
+          todos={todos} 
+          setTodos={setTodos} 
+          setEditTodo={setEditTodo}
+          />
         
         </div>    
       </div>     
